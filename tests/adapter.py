413af438af3dadec577a5766b11465445bc638c0
aduquet <duquet@ut.ee>
2021-11-04 13:10:32 +0100

    adapter

--- tests/adapter.py
+++ tests/adapter.py
@@ -5,18 +5,17 @@ import pandas as pd
 
 sys.path.append("C://Users//duquet//Documents//GitHub//toyExample//")
 
-def getState(self, a):
+def getState(input):
     
     global df_main
     df_main = pd.DataFrame()
   
     methodsList = ['testID', 'instanceID', 'get', 'avg', 'first', 'last']
-
     df_main = pd.DataFrame(columns=methodsList, index=None)
     
-    df_main['get'] = [a.gett()]
-    df_main['avg'] = a.avg()
-    df_main['first'] = a.first()
-    df_main['last'] = a.last()
-    # print(df_main)
+    df_main['get'] = [input.gett()]
+    df_main['avg'] = input.avg()
+    df_main['first'] = input.first()
+    df_main['last'] = input.last()
+
     return df_main
