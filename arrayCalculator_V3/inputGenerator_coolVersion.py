01b63f8263ce55a7eb414ca1ee415e3b669a9d50
aduquet <duquet@ut.ee>
2022-03-22 15:38:05 +0200

    update

--- arrayCalculator_V3/inputGenerator_coolVersion.py
+++ arrayCalculator_V3/inputGenerator_coolVersion.py
@@ -1,35 +1,30 @@
 import random
-
+import time 
 #this fuzzer function was taken from fuzzingbook.org
 #A string of up to max_length characters in the range [char_start, char_start + char_range)
 def fuzzer(max_length, char_start, char_range) -> str:
     
     string_length = random.randrange(0, max_length + 1)
-    # print(string_length)
+
     out = ""
     for i in range(0, string_length):
-        # print(i, string_length)
         out += chr(random.randrange(char_start, char_start + char_range))
     return out
-
-def converInt(numb):
     
-    auxList = []
+def saveArraysInputs(numb, fileName):
     
-    for i in numb:
-        # numDigits = random.randrange(0,2) + 1
-        # if numDigits == 2:
-        #     pos = numb.index(i)
-            
-        #     print('Pos', pos)
-        #     auxList.append(i)
-        # print(i)
-        auxList.append(i)
-    #print(random.randrange(0, len(numb)))
-    return(auxList)
+    with open(fileName, "a") as f:
+        f.write(numb + '\n')
 
-out = fuzzer(20, ord('0'), 10)
-out2 = converInt(out)
-print(out2)
-print(out)
+start_time = time.time()
+end_time = 0
+while end_time <= 5 :
+    
+    out = fuzzer(20, ord('0'), 10)
+    out2 = saveArraysInputs(out)
+    print(out2)
+    print(out)
+    end_time = time.time() - start_time
+    print(end_time)
+    
 #print(random.randrange(0,100))
