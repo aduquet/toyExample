a14813423fbf50b2405ebaeba3f691f20362482c
aduquet <duquet@ut.ee>
2022-03-23 19:33:13 +0200

    update

--- arrayCalculator_V3/traceGenerator.py
+++ arrayCalculator_V3/traceGenerator.py
@@ -2,25 +2,25 @@ import os
 import sys
 import pathlib
 import pandas as pd
-
+from arrayCalculator import *
 sys.path.append("C://Users//duquet//Documents//GitHub//toyExample//")
 
-def getState(input):
+def getState(input, inputID):
     
     global df_main
     df_main = pd.DataFrame()
-  
-    methodsList = ['inputID', 'inputValue','isEmpty', 'getSize', 'getAll', 'getFirst', 
-                   'getSecondLast', 'getLast', 'avg']
-    df_main = pd.DataFrame(columns=methodsList, index=None)
-    
-    df_main['isEmpty'] = input.isEmpty()
-    df_main['getSize'] = input.getSize()
-    df_main['getAll'] = [input.getAll()]
-    df_main['getFirst'] = input.getFirst()
-    df_main['getSecondLast'] = input.getSecondLast()
-    df_main['getLast'] = input.getLast()
-    df_main['avg'] = input.avg()
+    dict = {'inputID': inputID, 
+            'input': input,
+            'isEmpty': input.isEmpty(), 
+            'getSize': input.getSize(),
+            'getAll': input.getAll(), 
+            'getFirst': input.getFirst(), 
+            'getSecondLast': input.getSecondLast(), 
+            'getLast': input.getLast(), 
+            'avg': input.avg()}
+    df_main = df_main.append(dict, ignore_index = True)
+    key_list = list(dict.keys())
 
+    df_main.columns = key_list
 
     return df_main
